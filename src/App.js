1a708add372ad780b260c88713fdb75a4eefeca5
choihs0457 <choihs0457@gmail.com>
2023-04-03 17:25:13 +0900

    lecture 3.2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button, { ButtonTypes } from './components/Button';
-
 export default function App() {
   const [result, setResult] = useState(0);
-
   return (
     <View style={styles.container}>
       <View style={styles.resultContainer}>
         <Text style={styles.result}>{result}</Text>
       </View>
-
       <View style={styles.buttonContainer}>
         <Text>button</Text>
-      </View>
-
-      <View style={{ flex: 2, backgroundColor: 'gray' }}>
-        <Text>view</Text>
       </View>
 
       <StatusBar style="auto" />
@@ -27,22 +20,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    backgroundColor: '#ffffff',
   },
   resultContainer: {
     flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
     backgroundColor: '#000000',
   },
   buttonContainer: {
-    flex: 2,
+    flex: 1,
     backgroundColor: 'skyblue',
   },
   result: {
     color: '#ffffff',
     fontSize: 60,
     fontWeight: '700',
+    paddingBottom: 30,
+    paddingRight: 30,
   },
 });
