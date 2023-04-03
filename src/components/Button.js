b5e07d09e76c203d84b81224a9cc3804b465e0e3
choihs0457 <choihs0457@gmail.com>
2023-04-03 15:50:55 +0900

    start rn-calc

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import { Pressable, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+const Button = ({ title }) => {
+  return (
+    <Pressable
+      onPressIn={() => console.log('In')}
+      onPressOut={() => console.log('Out')}
+      onPress={() => console.log('onPress')}
+      onLongPress={() => console.log('Long')}
+      delayLongPress={2000}
+      style={({ pressed }) => [
+        { backgroundColor: 'red' },
+        pressed && { backgroundColor: 'orange', opacity: 0.3 },
+      ]}
+    >
+      <Text>{title}</Text>
+    </Pressable>
+  );
+};
+
+Button.defaultProps = {
+  title: 'Default',
+};
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+export default Button;
