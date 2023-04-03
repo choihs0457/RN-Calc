1cc354b6b4bfb7c9c811ef44999f9ebb4c0c41f2
choihs0457 <choihs0457@gmail.com>
2023-04-03 16:41:00 +0900

    lecture 2.7

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './components/Button';
+import { StyleSheet, View } from 'react-native';
+import Button, { ButtonTypes } from './components/Button';
 
 export default function App() {
-  const isError = true;
-
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>RN Calc App</Text>
       <Button
-        title="button"
-        color="red"
-        onPress={() => console.log('click!')}
+        title="1"
+        onPress={() => {}}
+        buttonStyle={{ width: 100, height: 100 }}
+        buttonType={ButtonTypes.NUMBER}
       />
-
-      <Button />
+      <Button
+        title="0"
+        onPress={() => {}}
+        buttonStyle={{ width: 200, height: 100 }}
+      />
+      <Button
+        title="="
+        onPress={() => {}}
+        buttonStyle={{ width: 100, height: 100 }}
+        buttonType={ButtonTypes.OPERATOR}
+      />
 
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    fontSize: 30,
-    fontWeight: '700',
-    color: 'green',
   },
 });
